a1134aee79db8d7f00ddba2bfb4cd79da0bec261
LOGESHWARAN R <96953357+Logeshwaranr0902@users.noreply.github.com>
2024-10-07 19:44:46 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -6,12 +6,11 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-import { productsData } from './api/api';
-import Signin from './pages/Signin';
+import { productsData } from "./api/api";
 
 const Layout = () => {
   return (
@@ -23,18 +22,16 @@ const Layout = () => {
   );
 };
 function App() {
-  const router = createBrowserRouter(createRoutesFromElements(
-
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} loader={productsData}></Route>
-      <Route path="/cart" element={<Cart />}></Route>
-    </Route>
-
-
-
-  ))
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} loader={productsData}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Route>
+    )
+  );
   return (
-    <div className='font-bodyFont bg-gray-100'>
+    <div className="font-bodyFont bg-gray-100">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
